33b883debba878c0a8834f7b7a53a4897fef708d
Alexandre Grosse <grssalex@mac.home>
2025-02-04 23:16:04 +0100

    [FIX] gh pages deploy failed

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,6 @@ import { defineConfig } from "astro/config";
 
 export default defineConfig({
   site: "https://grssalex.github.io/website-agence/",
-  base: "/website-agence/", 
+  base: "/website-agence/",
   output: "static",
 });
