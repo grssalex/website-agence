7d3bf1451ffeb201aa23b5a0d48ebb59e5482899
Alexandre Grosse <grssalex@MacBook-Pro-de-Alexandre.local>
2025-02-05 18:54:38 +0100

    [ADD] menu responsive

--- src/scripts/menu.js
+++ src/scripts/menu.js
@@ -2,8 +2,11 @@ document.addEventListener('DOMContentLoaded', () => {
     const menuToggle = document.querySelector('.menu-toggle');
     const nav = document.querySelector('.nav');
     
+    nav.classList.remove('active');
+    menuToggle.classList.remove('active');
+    
     menuToggle.addEventListener('click', () => {
-      nav.classList.toggle('active');
-      menuToggle.classList.toggle('active');
+        nav.classList.toggle('active');
+        menuToggle.classList.toggle('active');
     });
-  });
+});
